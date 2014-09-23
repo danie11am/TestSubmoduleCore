386f45564288f8385dde2d2a7be3671d98b3b1d3
Daniel Lam <im@daniellam.name>
2014-09-24 01:19:26 +1000

    Initial Commit

---
+++ TestSubmoduleCore/TestSubmoduleCore.h
@@ -0,0 +1,13 @@
+//
+//  TestSubmoduleCore.h
+//  TestSubmoduleCore
+//
+//  Created by Daniel Lam on 24/09/2014.
+//  Copyright (c) 2014 Daniel Lam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestSubmoduleCore : NSObject
+
+@end
---
+++ TestSubmoduleCore/TestSubmoduleCore.m
@@ -0,0 +1,13 @@
+//
+//  TestSubmoduleCore.m
+//  TestSubmoduleCore
+//
+//  Created by Daniel Lam on 24/09/2014.
+//  Copyright (c) 2014 Daniel Lam. All rights reserved.
+//
+
+#import "TestSubmoduleCore.h"
+
+@implementation TestSubmoduleCore
+
+@end
